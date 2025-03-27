f2225b5623add464ce1619c8ab274c7e2a775a13
linder3hs <linderhassinger00@gmail.com>
2025-03-26 21:57:56 -0500

    feat: add promise to read file

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const markdownInput = document.querySelector("#markdown-input");
 // section preview
 const previewSection = document.querySelector("#preview-section");
 const changeBoldOrCursive = document.querySelector("#change-bold-or-cursive");
+const inputFile = document.querySelector("#input-file");
+const loading = document.querySelector("#loading");
 
 let state = false;
 let currentSelectedText = "";
@@ -19,7 +21,60 @@ function renderOnInput() {
   getTextFromTextArea(convertToHtml);
 }
 
+function readFileAsPromise(file) {
+  return new Promise((resolve, reject) => {
+    const lector = new FileReader();
+
+    setTimeout(() => {
+      lector.readAsText(file);
+
+      lector.onload = function () {
+        resolve(lector.result);
+      };
+
+      lector.onerror = function () {
+        reject("Error al leer el archivo");
+      };
+    }, 2000);
+  });
+}
+
 const debounceRender = debounce(renderOnInput, 300);
+
+// inputFile.addEventListener("change", function (event) {
+//   const archivo = event.target.files[0];
+
+//   loading.classList.remove("hidden");
+
+//   readFileAsPromise(archivo)
+//     .then((value) => {
+//       console.log("TEXTO:", value);
+//       markdownInput.value = value;
+//       debounceRender();
+//     })
+//     .catch((error) => {
+//       console.log("Error", error);
+//     })
+//     .finally(() => {
+//       loading.classList.add("hidden");
+//     });
+// });
+
+inputFile.addEventListener("change", async function (event) {
+  const archivo = event.target.files[0];
+
+  loading.classList.remove("hidden");
+
+  try {
+    const text = await readFileAsPromise(archivo);
+    markdownInput.value = text;
+    debounceRender();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.classList.add("hidden");
+  }
+});
 
 markdownInput.addEventListener("input", debounceRender);
 
